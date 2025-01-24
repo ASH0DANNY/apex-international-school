98aed9337946e87b0d4fc94b5a0df6b76bf120c3
ASH0DANNY <115711791+ASH0DANNY@users.noreply.github.com>
2025-01-24 11:10:54 +0530

    Firebase config content removed

--- firebseConfig.ts
+++ firebseConfig.ts
@@ -1,18 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyATp88VZZOiu2MSbHa-2N082DHyfLSnDCs",
-    authDomain: "apx-international-dev.firebaseapp.com",
-    projectId: "apx-international-dev",
-    storageBucket: "apx-international-dev.firebasestorage.app",
-    messagingSenderId: "203374079822",
-    appId: "1:203374079822:web:da315e750942dced9a381f",
-    measurementId: "G-8JYP9S6SE6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Firestore
-export const db = getFirestore(app);
 
